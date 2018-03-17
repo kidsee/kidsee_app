7672d4b0cab8ad24d6a535da0706d89dfbdc36ce
lucashorward <lucas.horward@gmail.com>
2018-03-17 14:14:49 +0100

    Added post detail view, post list navigation to detail

---
+++ src/pages/post/post.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PostPage } from './post';
+import {ComponentsModule} from "../../components/components.module";
+
+@NgModule({
+  declarations: [
+    PostPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PostPage),
+      ComponentsModule
+  ],
+})
+export class PostPageModule {}
