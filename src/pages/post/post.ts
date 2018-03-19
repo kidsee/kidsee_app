00ee9ad225cb4e0fc73d96d852fada18bf340642
Dave <pjhkaste@avans.nl>
2018-03-19 13:42:43 +0100

    readd posts

---
+++ src/pages/post/post.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+    selector: 'page-post',
+    templateUrl: 'post.html',
+})
+export class PostPage {
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+}
