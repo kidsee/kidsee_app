446b214cefc761bbd574dfa927a4f5c89267bfad
lucashorward <lucas.horward@gmail.com>
2018-03-21 00:35:06 +0100

    Updated properties to hasmany/belongsto where appropriate

--- src/app/models/comment.ts
+++ src/app/models/comment.ts
@@ -1,16 +1,16 @@
-import {Attribute, JsonApiModel, JsonApiModelConfig} from "angular2-jsonapi";
-import {User} from "./user";
-import {Post} from "./post";
-import {ContentType} from "./contentType";
+import { Attribute, JsonApiModel, JsonApiModelConfig, BelongsTo } from "angular2-jsonapi";
+import { User } from "./user";
+import { Post } from "./post";
+import { ContentType } from "./contentType";
 
 @JsonApiModelConfig({
     type: 'comments'
 })
 export class Comment extends JsonApiModel {
-    @Attribute()
+    @BelongsTo()
     user: User;
 
-    @Attribute()
+    @BelongsTo()
     post: Post;
 
     @Attribute()
