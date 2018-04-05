4a7535151574418d551e9024c75b7a5c1c3e7fe6
Martijn Coomans <mcoomans@avans.nl>
2018-04-05 12:54:01 +0200

    added validation to register

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -11,7 +11,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LoginPage {
   private loading: Loading;
-  
+
   registerCredentials = {identification: '', password: ''};
 
   constructor(
