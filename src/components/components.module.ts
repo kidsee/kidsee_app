63cee612fd10bc36441eb37c169217ce0adc9f60
Martijn Coomans <mcoomans@avans.nl>
2018-04-17 12:35:07 +0200

    replaced simple button with actual timeline

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { BaseHeaderComponent } from './base-header/base-header';
 import { TimelineContainerComponent } from './timeline-container/timeline-container';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [BaseHeaderComponent,
     TimelineContainerComponent],
-  imports: [IonicModule],
+  imports: [IonicModule, TranslateModule],
   exports: [BaseHeaderComponent,
     TimelineContainerComponent]
 })
