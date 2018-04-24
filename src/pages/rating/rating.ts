bf491b04aaa5574a0de0f9f25f46d6fca311345c
Martijn Coomans <mcoomans@avans.nl>
2018-04-24 21:36:15 +0200

    begin top 10 lijst

--- src/pages/rating/rating.ts
+++ src/pages/rating/rating.ts
@@ -7,9 +7,42 @@ import { IonicPage } from 'ionic-angular';
   templateUrl: 'rating.html',
 })
 export class RatingPage {
+  protected themes :string[] = ['Eten', 'Activiteiten', 'Natuur'];
+  protected selectedTheme :string;
+  protected items :string[];
 
-  constructor() { }
+  constructor() {
+    this.selectedTheme = this.themes[0];
+    this.selectTheme(this.selectedTheme);
+  }
 
-
-
+  selectTheme(theme: string) {
+    this.selectedTheme = theme;
+    if(theme === 'Eten') {
+      this.items = [
+        'McDonalds',
+        'Sakana',
+        'Picaso',
+        'Subway'
+      ]
+    } else if (theme === 'Activiteiten') {
+      this.items = [
+        'Activiteit 1',
+        'Activiteit 2',
+        'Activiteit 3',
+        'Activiteit 4',
+        'Activiteit 5',
+        'Activiteit 6',
+        'Activiteit 7',
+        'Activiteit 8',
+        'Activiteit 9',
+        'Activiteit 10',
+      ]
+    } else if (theme === 'Natuur') {
+      this.items = [
+        'Het Bossche Broek',
+        'Zuiderplas'
+      ]
+    }
+  }
 }
