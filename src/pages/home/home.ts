3fa0502947d721bea23305829f024f867e899b86
JacobMeister <maarten.jacobs15@gmail.com>
2018-05-04 09:17:34 +0200

    plain radial menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from "../../app/models/user";
 import { AuthServiceProvider } from "../../providers/auth-service/auth-service";
-import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng} from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng, MarkerIcon } from '@ionic-native/google-maps';
 import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { LocationServiceProvider } from "../../providers/location-service/location-service";
@@ -19,6 +19,17 @@ export class HomePage {
   private map: GoogleMap;
   private locations: Location[] = [];
   protected loaded = false;
+  public radialVisible = false;
+  public menuVisible = true;
+  public radialClasses = "front_buttons animated zoomIn";
+  public profileButtonClasses = "profile_button animated fadeIn";
+  public settingsButtonClasses = "settings_button animated fadeIn";
+  public containerClasses = "animated fadeIn";
+  public buttonGridClasses = "button_grid animated fadeIn";
+  public radialAmount = 5;
+  public radialIcons = ["assets/imgs/photo-icon.png", "assets/imgs/question-icon.png", "assets/imgs/puzzle-icon.png", "assets/imgs/rating-icon.png", "assets/imgs/info-icon.png"];
+  public radialURLs = [" ", " ", " ", " ", " "];
+  public currentSelectedLocation : Location;
 
   constructor(
     public navController: NavController,
@@ -27,13 +38,17 @@ export class HomePage {
     private androidPermissions: AndroidPermissions,
     private screenOrientation: ScreenOrientation,
     private locationService: LocationServiceProvider,
-    private platform: Platform
-  )
-    {
-
+    private platform: Platform,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+    platform.registerBackButtonAction(() => {
+      if(this.radialVisible == true){
+        this.closeMarkerMenu();
+      }
+    },1);
   }
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     this.authServiceProvider.fetchCurrentUser().then(user => {
       this.user = user;
     });
@@ -49,7 +64,7 @@ export class HomePage {
     this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
       //gets location permission
       this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.LOCATION).then(result => {
-      if(!result.hasPermission){
+      if(!result.hasPermission) {
         this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.LOCATION]);
       }},err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.LOCATION));
       //sets more map properties
@@ -66,21 +81,98 @@ export class HomePage {
 
   }
 
-  setMarkers(){
+  setMarkers() {
     this.locationService.locations().then((res) => {
       this.locations = res as Location[];
-        this.locations.forEach(location => {
-
-          this.map.addMarker({
-            position: {lat: location.lat, lng: location.lon},
-            title: location.name
-
-          });
-
-        });
-      }
+      this.locations.forEach(location => {
+        //if location type
+        //change different icons
+        this.map.addMarker({
+          position: { lat: location.lat, lng: location.lon },
+          title: location.name,
+          icon: {
+            url: "assets/imgs/Coins.png",
+            size: {
+              width: 40,
+              height: 40
+            }
+          }
+        }).then(marker => {
+          marker.on(GoogleMapsEvent.MARKER_CLICK)
+            .subscribe(() => {
+              this.currentSelectedLocation = location;
+              console.log(this.currentSelectedLocation);
+              console.log("click");
+              this.setRadialLayout(location);
+              this.openMarkerMenu();
+            });
+        })
+      });
+    }
     );
+  }
 
+  
+
+  public setRadialLayout(location: Location) {
+    //check what buttons are needed
+    //set icons
+    this.radialIcons[0] = "assets/imgs/photo-icon.png";
+    this.radialIcons[1] = "assets/imgs/question-icon.png";
+    this.radialIcons[2] = "assets/imgs/puzzle-icon.png";
+    this.radialIcons[3] = "assets/imgs/rating-icon.png";
+    this.radialIcons[4] = "assets/imgs/info-icon.png";
+    this.radialURLs[0] = "PhotoandvideoPage";
+    this.radialURLs[1] = "QuestionandanswerPage";
+    this.radialURLs[2] = "PuzzlePage";
+    this.radialURLs[3] = "RatingPage";
+    // this.radialURLs[4] = "info";
+
+  }
+
+  public openMarkerMenu() {
+    this.map.setAllGesturesEnabled(false);
+    let timeout = setTimeout(() => {
+      this.profileButtonClasses = "profile_button animated fadeOut";
+      this.settingsButtonClasses = "settings_button animated fadeOut";
+      this.containerClasses = "animated fadeOut";
+      this.buttonGridClasses = "button_grid animated fadeOut";
+      this.menuVisible = false;
+      this.changeDetectorRef.detectChanges();
+      this.menuVisible = true;
+      this.radialVisible = true;
+      this.changeDetectorRef.detectChanges();
+      let timeout = setTimeout(() => {
+        this.menuVisible = false;
+        this.changeDetectorRef.detectChanges();
+      }, 1000);
+    }, 300);
+  }
+
+  public closeMarkerMenu() {
+
+    this.radialClasses = "front_buttons animated zoomOut";
+    this.radialVisible = false;
+    this.changeDetectorRef.detectChanges();
+    this.radialVisible = true;
+    this.profileButtonClasses = "profile_button animated fadeIn";
+    this.settingsButtonClasses = "settings_button animated fadeIn";
+    this.containerClasses = "animated fadeIn";
+    this.buttonGridClasses = "button_grid animated fadeIn";
+    this.menuVisible = true;
+    this.changeDetectorRef.detectChanges();
+    let timeout = setTimeout(() => {
+      this.radialVisible = false;
+      this.changeDetectorRef.detectChanges();
+    }, 1000);
+    this.map.setAllGesturesEnabled(true);
+    this.radialClasses = "front_buttons animated zoomIn";
+  }
+
+  public redirect(buttonNo: number) {
+    let direction = this.radialURLs[buttonNo-1];
+    this.navController.push(this.radialURLs[buttonNo-1]);
+    
   }
 
   public settings() {
