40f68e9a44cca96c5d316c5e4da9ae59395d3c38
lucashorward <lucas.horward@gmail.com>
2018-05-06 20:48:53 +0200

    Question list visualisation

--- src/pages/questionandanswer/questionandanswer.module.ts
+++ src/pages/questionandanswer/questionandanswer.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { QuestionandanswerPage } from './questionandanswer';
+import { TranslateModule } from "@ngx-translate/core";
+import { ComponentsModule } from "../../components/components.module";
 
 @NgModule({
   declarations: [
@@ -8,6 +10,13 @@ import { QuestionandanswerPage } from './questionandanswer';
   ],
   imports: [
     IonicPageModule.forChild(QuestionandanswerPage),
+    TranslateModule,
+    ComponentsModule,
   ],
+  exports: [
+    QuestionandanswerPage
+  ]
 })
-export class QuestionandanswerPageModule {}
+export class QuestionandanswerPageModule {
+
+}
--- src/pages/questionandanswer/questionandanswer.ts
+++ src/pages/questionandanswer/questionandanswer.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { PostService } from "../../providers/post-service/post-service";
+import { Post } from "../../app/models/post";
 
 @IonicPage()
 @Component({
@@ -7,8 +9,45 @@ import { IonicPage } from 'ionic-angular';
   templateUrl: 'questionandanswer.html',
 })
 export class QuestionandanswerPage {
+  protected posts: Post[] = [];
+  private page = 0;
 
-  constructor() { }
+  constructor(
+    private navController: NavController,
+    private postService: PostService
+  ) { }
 
+  protected goToPost(post: Post) {
+    this.navController.push('PostPage', {post: post});
+  }
 
+  ionViewDidEnter() {
+    this.fetchNewPage();
+  }
+
+  private fetchNewPage() {
+    this.page++;
+    this.postService.posts({page: this.page}).subscribe(
+      posts => {
+        posts.getModels().forEach(post => {
+          this.posts.push(post);
+        });
+      }
+    )
+  }
+
+  protected doInfinite(infiniteScroll) {
+    setTimeout(() => {
+      this.fetchNewPage();
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+  protected createPost(){
+    this.navController.push('CreatePostPage');
+  }
+
+  protected back(){
+    this.navController.pop();
+  }
 }
