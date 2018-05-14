eefda73a65b160c57873a2fe45c48bf3bc3afb93
lucashorward <lucas.horward@gmail.com>
2018-05-14 17:04:45 +0200

    Added comment rating

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Post } from "../../app/models/post";
+import { RatingServiceProvider } from "../../providers/rating-service/rating-service";
 
 @IonicPage()
 @Component({
@@ -12,11 +13,17 @@ export class PostPage {
 
   constructor(
     private navController: NavController,
-    private navParams: NavParams
+    private navParams: NavParams,
+    private ratingService: RatingServiceProvider
   ) { }
 
   ionViewDidEnter() {
     this.post = this.navParams.get('post');
+    this.post.comments.forEach(comment => {
+      this.ratingService.getTotalRating(comment).then(rating => {
+        comment.rating = rating;
+      });
+    });
   }
 
   protected back() {
